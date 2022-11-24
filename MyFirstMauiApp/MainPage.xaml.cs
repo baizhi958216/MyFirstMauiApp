ad28c395c6b217b7acbeb266cbca35a4965b889f
baizhi958216 <1475289190@qq.com>
2022-11-24 23:21:48 +0800

    Todolist.

---
+++ MyFirstMauiApp/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MyFirstMauiApp.ViewModel;
+
+namespace MyFirstMauiApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage(MainViewModel vm)
+	{
+        InitializeComponent();//定义在xaml的组件会在此时实例化
+        BindingContext = vm;
+    }
+}
+
---
+++ MyFirstMauiApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace MyFirstMauiApp.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    public MainViewModel()
+    {
+        Items = new ObservableCollection<string>();
+    }
+
+    [ObservableProperty]
+    ObservableCollection<string> items;
+
+    [ObservableProperty]
+    string text;
+
+    [RelayCommand]
+    void Add()
+    {
+        if (string.IsNullOrEmpty(text))
+            return;
+        Items.Add(text);
+        //Add out item
+        Text = string.Empty;
+    }
+
+    [RelayCommand]
+    void Delete(string s)
+    {
+        if (Items.Contains(s))
+        {
+            Items.Remove(s);
+        }
+    }
+}
